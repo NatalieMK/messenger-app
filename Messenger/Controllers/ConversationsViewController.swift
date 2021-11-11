f3d35ed060221ca62e0c61a52ebb5c66808f6e17
NatalieMK <nataliemadison17@gmail.com>
2021-11-10 21:34:44 -0500

    Initial Commit

---
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Natalie on 11/10/21.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn{
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+
+}
+
